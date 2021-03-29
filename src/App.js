6ffb24448c17e8b1c622bfbf112d424bea68b51c
Daniel-Sogbey <mathematics06physics@gmail.com>
2021-03-28 18:47:16 -0700

    added video as background

--- src/App.js
+++ src/App.js
@@ -3,18 +3,40 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 // import About from "./components/About/About";
 // import Footer from "./components/Footer/Footer";
-import "./App.css";
+import classes from "./App.css";
+import video from "./videos/pied-piper-video.mp4";
 
 class App extends Component {
+	// componentDidMount() {
+	// if (this.video) {
+	// this.video.addEventListener("loadeddata", () => {});
+	// }
+	// }
+
+	// componentWillUnmount() {
+	// if (this.video) {
+	// this.video.removeEventListener("loadeddata", () => {});
+	// }
+	// }
+	//
 	render() {
+		// const src = "./videos/pied-piper-video.mp4";
 		return (
 			<Router>
-				<div>
-					<Navbar />
-					<Switch>
-						{/* <Route exact path="/about" component={About} /> */}
-					</Switch>
+				<div className={classes.content}>
+					<div className={classes.video}>
+						<video autoPlay loop muted playsInline>
+							<source src={video} type="video/mp4" />
+						</video>
+					</div>
+					<div>
+						<Navbar />
+						<Switch>
+							{/* <Route exact path="/about" component={About} /> */}
+						</Switch>
+					</div>
 				</div>
+
 				{/* <Footer /> */}
 			</Router>
 		);
